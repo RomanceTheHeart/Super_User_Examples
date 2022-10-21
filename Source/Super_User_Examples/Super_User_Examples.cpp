cb6489139949f6ce32ff258f19e7dcd47f6ab2f9
RomanceTheHeart <gerardpaul@outlook.com>
2022-10-21 10:10:55 -0400

    added two more modules in Unreal

---
+++ Source/Super_User_Examples/Super_User_Examples.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Super_User_Examples.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Super_User_Examples, "Super_User_Examples" );
