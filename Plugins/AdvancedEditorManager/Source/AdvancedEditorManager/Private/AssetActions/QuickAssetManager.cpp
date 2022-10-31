e0e9c910f13d03fd6e92b564ed4d0e623d365221
RomanceTheHeart <gerardpaul@outlook.com>
2022-10-30 23:58:15 -0400

    Created a master disolve and master material.

--- Plugins/AdvancedEditorManager/Source/AdvancedEditorManager/Private/AssetActions/QuickAssetManager.cpp
+++ Plugins/AdvancedEditorManager/Source/AdvancedEditorManager/Private/AssetActions/QuickAssetManager.cpp
@@ -19,12 +19,14 @@ void UQuickAssetManager::DubplicateAssets(int32 NumberOfDuplicates)
 		Print("Please Enter a Valid Number",FColor::Blue);
 		return;
 	}	
-	
+
+	// Create an AssetData array. 
 	TArray<FAssetData> SelectedAssetData = UEditorUtilityLibrary::GetSelectedAssetData();
 	uint32 counter{0};
+	// iterate through it.
 	for(const FAssetData& AssetData:SelectedAssetData)
 	{
-		 
+		 //iterate again to create a new file name and path location.
 		for(int32 i=0; NumberOfDuplicates; i++)
 		{
 			
@@ -32,10 +34,10 @@ void UQuickAssetManager::DubplicateAssets(int32 NumberOfDuplicates)
 			const FString NewDuplicatedAssetName = AssetData.AssetName.ToString() + TEXT("_")+ FString::FromInt(i);
 			const FString NewPath = FPaths::Combine(AssetData.PackagePath.ToString(),NewDuplicatedAssetName);
 
-
+			//check for valid path. if valid duplicate path and append a new name to each asset. 
 			if(UEditorAssetLibrary::DuplicateAsset(SourceAssetPath,NewPath))
 			{
-
+				
 				UEditorAssetLibrary::SaveAsset(NewPath,false);
 				//Print(("%s",SelectedAssetData),FColor::Emerald);
 				
