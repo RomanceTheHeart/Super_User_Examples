169304785302a39eacd0f463f9878ba12f9d94e3
RomanceTheHeart <gerardpaul@outlook.com>
2022-11-13 13:13:03 -0500

    Implemented MyActor Actor

--- Source/Super_User_Examples/MyActor.cpp
+++ Source/Super_User_Examples/MyActor.cpp
@@ -2,12 +2,16 @@
 
 
 #include "MyActor.h"
+#include "Components/StaticMeshComponent.h"
 
 // Sets default values
 AMyActor::AMyActor()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	Mesh_1 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("RootMeshComonent"));
+	auto MeshAsset = ConstructorHelpers::FObjectFinder<UStaticMesh>(TEXT("StaticMesh'/Game/StarterContent/Shapes/Shape_Cube.Shape_Cube'"));
+	if(MeshAsset.Object != nullptr){Mesh_1->SetStaticMesh(MeshAsset.Object);}
 
 }
 
@@ -15,7 +19,7 @@ AMyActor::AMyActor()
 void AMyActor::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 }
 
 // Called every frame
