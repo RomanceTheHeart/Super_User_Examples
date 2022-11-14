c62066e5861c00ac9aa24a733ca0a8e42a9b9560
RomanceTheHeart <gerardpaul@outlook.com>
2022-11-14 14:50:19 -0500

    Made changes to spawn function but not quit working

--- Source/Super_User_Examples/MiniProjects.cpp
+++ Source/Super_User_Examples/MiniProjects.cpp
@@ -15,26 +15,38 @@ void AMiniProjects::BeginPlay()
 
 	FTransform SpawnLocation;
 
-	const int32 blockindex{8};
+	const int32 blockindex{2};
 	const int32 BlockNumber{blockindex * blockindex};
-	const float OffSetAmount{100.0};
-	float Z_OffsetAmount{3.5};
+	float OffSetAmount{300.0};
+	float Z_OffsetAmount{0.0f};
 	//float worldtime = GetGameTimeSinceCreation();
 	FActorSpawnParameters SpawnParameters;
-	for (int32 index{0}; index < BlockNumber; index ++)
+
+	for (int32 index{0}; index < BlockNumber * BlockNumber; index ++)
 	{
 		const float X_Offset{((index / BlockNumber) * OffSetAmount)};
 		const float Y_Offset = {((index % BlockNumber) * OffSetAmount)};
 		const float Z_Offset = {((index % BlockNumber) * Z_OffsetAmount)};
 		const FVector newspawnvector = FVector(X_Offset, Y_Offset, Z_Offset);
-		if(BlockNumber/blockindex <= blockindex){Z_OffsetAmount = 100.0f;}
+
 		if (GetWorld() != nullptr)
 			SpawnedActor = GetWorld()->SpawnActor<AMyActor>(AMyActor::StaticClass(), newspawnvector, FRotator(0, 0, 0),
 			                                                SpawnParameters);
+		if (index <= FMath::Pow(BlockNumber, 2))
+		{
+		
+			Z_OffsetAmount += 10;
+			OffSetAmount += 10;
+		}
+		for (int32 index_j{BlockNumber}; index_j < (BlockNumber); index_j ++)
+		{
+			const FVector newspawnvector_2 = FVector(X_Offset, Y_Offset, Z_Offset);
+			SpawnedActor = GetWorld()->SpawnActor<AMyActor>(AMyActor::StaticClass(), newspawnvector_2,
+															FRotator(0, 0, 0),
+															SpawnParameters);
+		}
+		GetWorldTimerManager().SetTimer(DestroyTimer, this, &AMiniProjects::DestroyActor, 10);
 	}
-
-
-	GetWorldTimerManager().SetTimer(DestroyTimer, this, &AMiniProjects::DestroyActor, 10);
 }
 
 void AMiniProjects::DestroyActor()
